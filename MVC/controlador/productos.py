c1bde484004a065fa0875373ad9f47ec295dba3e
Pablo Montoya <montoya_pablo@outlook.com.ar>
2022-10-14 20:49:23 -0300

    actualizacion de MVC

---
+++ MVC/controlador/productos.py
@@ -0,0 +1,10 @@
+#instanciar un nuevo producto
+
+#eliminar posteo
+
+#concretar truque
+
+#pausar posteo por denuncia/usuario
+
+#
+
---
+++ MVC/controlador/usuario.py
@@ -0,0 +1,9 @@
+#cambio de correo electronico
+
+#cambio de contraseña
+
+#baja de de la cuenta
+
+#validar identidad
+
+#carga de foto

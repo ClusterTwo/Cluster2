3a7fa9f31a244fbb06935b8959842f9d47cb3e40
PABLO MATIAS JOSE MONTOYA <106557347+MontoyaPablo@users.noreply.github.com>
2022-10-14 21:05:37 -0300

    actualizacion

---
+++ MVC/controlador/dataBase.py
@@ -0,0 +1,18 @@
+#generar base de datos
+
+#cargar usuario
+
+#consultas de usuarios
+
+#update de usuario
+
+#delete users
+
+#carga de productos
+
+#update de productos
+
+#consultas de productos
+
+#delete productos
+

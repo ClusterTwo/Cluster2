521ac7ffa90319e9e89368f4e46ce2ddd79a5726
Pablo Montoya <montoya_pablo@outlook.com.ar>
2022-10-30 09:32:07 -0300

    conexion de base de datos

--- MVC/controlador/productos.py
+++ MVC/controlador/productos.py
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 #instanciar un nuevo producto
 
 #eliminar posteo
@@ -9,15 +9,4 @@
 
 #
 
-=======
-#instanciar un nuevo producto
 
-#eliminar posteo
-
-#concretar truque
-
-#pausar posteo por denuncia/usuario
-
-#
-
->>>>>>> bd7a3b46c72e461efe4132d840d79de3e1eba167

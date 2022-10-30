95a1ad946191603fde9e2db00973f9ad69083f74
Pablo Montoya <montoya_pablo@outlook.com.ar>
2022-10-30 09:32:07 -0300

    conexion de base de datos

--- MVC/controlador/dataBase.py
+++ MVC/controlador/dataBase.py
@@ -1,7 +1,59 @@
-<<<<<<< HEAD
+from readline import insert_text
+import sqlite3 as sql
+from tkinter import _Cursor
+
 #generar base de datos
+def creadorDeBase():
+    conn = sql.connect("retrueque.db")
+    conn.commit()
+    conn.close()
+
+#crear tablas
+def creadorDeTablas():
+    conn= sql.connect("retrueque.db")
+    cursor = conn.cursor()
+    cursor.execute(
+        """ CREATE TABLE usuario(
+
+            idUsuario int PRIMARY KEY NOT NULL AUTOINCREMENTAL,
+            email varchar (50) NOT NULL,
+            nombre varchar (100) NOT NULL,
+            apellido varchar(50) NOT NULL,
+            telefono varchar(15) NOT NULL,
+            password varchar(20) NOT NULL,
+            edad  date,
+            fotoPerfil  varbinary(2mb),
+            nivelUsario int NOT NULL)
+        """
+        """ CREATE TABLE producto(
+
+            idProducto  int PRIMARY KEY NOT NULL AUTOINCREMENTAL,
+            descripcion varchar(150) NOT NULL,
+            fotoProducto varbinary(2mb),
+            categoria varchar(20) NOT NULL,
+            interesDeIntercambio varchar(50))
+        """
+        """ CREATE TABLE trade(
+
+            idTransaccion int PRIMARY KEY NOT NULL AUTOINCREMENTAL,
+            tradeidUsuario int,
+            tradeidProducto int,
+            registro datetime, 
+            CONSTRAINT fk_idUsuario FOREIGN KEY (tradeidUsuario) 
+            REFERENCES usuario (idUsuario),
+            CONSTRAINT fk_idProducto FOREIGN KEY (tradeidProducto) 
+            REFERENCES producto (idProducto))
+        """
+    )
 
 #cargar usuario
+def creadorDeTablas():
+    conn= sql.connect("retrueque.db")
+    cursor = conn.cursor()
+    instanciaUsuario = f"INSERT INTO usuario VALUES ('{email}','{nombre}','{apellido}','{telefono}','{password}', {nivelUsuario} )"
+    cursor.execute(instanciaUsuario)
+    conn.commit()
+    conn.close
 
 #consultas de usuarios
 
@@ -16,24 +68,3 @@
 #consultas de productos
 
 #delete productos
-
-=======
-#generar base de datos
-
-#cargar usuario
-
-#consultas de usuarios
-
-#update de usuario
-
-#delete users
-
-#carga de productos
-
-#update de productos
-
-#consultas de productos
-
-#delete productos
-
->>>>>>> bd7a3b46c72e461efe4132d840d79de3e1eba167
--- MVC/controlador/usuario.py
+++ MVC/controlador/usuario.py
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 #cambio de correo electronico
 
 #cambio de contraseña
@@ -7,14 +7,3 @@
 
 #validar identidad
 
-=======
-#cambio de correo electronico
-
-#cambio de contraseña
-
-#baja de de la cuenta
-
-#validar identidad
-
->>>>>>> bd7a3b46c72e461efe4132d840d79de3e1eba167
-#carga de foto

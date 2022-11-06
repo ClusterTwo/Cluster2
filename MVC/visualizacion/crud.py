23bd6b2801e402da2e8f432185afeef987fac70a
Pablo Montoya <montoya_pablo@outlook.com.ar>
2022-11-06 17:44:46 -0300

    basededatos y crud

--- MVC/visualizacion/crud.py
+++ MVC/visualizacion/crud.py
@@ -1,4 +1,9 @@
-from MVC.controlador.dataBase import cargaDeUsuario
+from sys import path
+path.append('C:\\Users\\pablo\\Documents\\Ispc\\ClusterTwo\\Cluster2\\MVC')
+path.append('c:\\users\\pablo\\appdata\\local\\programs\\python\\python310\\lib\\site-packages')
+from controlador.dataBase import *
+from controlador.usuario import *
+
 
 cargaDeUsuario(
     email = input("Correo Electronico: "),
@@ -7,3 +12,13 @@ cargaDeUsuario(
     telefono = input("telefono: "),
     nivelUsuario = 0
 )
+
+cambioDeContraseña(
+        password=input("ingresar nueva contraseña: "),
+        idUsuario= 1
+        )
+
+cambioDeEmail(
+        email=input('ingrese el nuevo e-mail: '),
+        idUsuario= 1
+        )

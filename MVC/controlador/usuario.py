7676beac531d008aa051a08625d737054b851c10
PABLO MATIAS JOSE MONTOYA <106557347+MontoyaPablo@users.noreply.github.com>
2022-11-12 10:54:46 -0300

    usuario

--- MVC/controlador/usuario.py
+++ MVC/controlador/usuario.py
@@ -2,18 +2,42 @@ from sys import path
 path.append('c:\\users\\pablo\\appdata\\local\\programs\\python\\python310\\lib\\site-packages')
 import mysql.connector 
 
+#cargar usuario
+def cargaDeUsuario(
+        email, 
+        nombre, 
+        apellido, 
+        telefono, 
+        password, 
+        nivelUsuario):
+    connection=mysql.connector.connect(
+    host='localhost',
+    database='retrueque',
+    user='root',
+    password='')
+    cursor = connection.cursor()
+    insertDato = """INSERT INTO Usuario ( email, nombre, apellido, telefono, password, nivelUsuario) 
+                                VALUES ( %s, %s, %s, %s, %s, %s) """
+    record = ( email, nombre, apellido, telefono, password,nivelUsuario)
+    cursor.execute(insertDato, record)
+    connection.commit()
+    cursor.close()
+    connection.close()
+    
+
 #cambio de correo electronico
 def cambioDeEmail(email,idUsuario):
     try:
-        connection = mysql.connector.connect(host='localhost',
-        database='retrueque.sql',
-        user='admin',
-        password='admin')
+        connection = mysql.connector.connect(
+            host='localhost',
+            database='retrueque',
+            user='root',
+            password='')
 
-        mySql_insert_query = f" UPDATE email SET email='{email}' WHERE idUsuario={idUsuario};"
+        updateDato = f" UPDATE email SET email='{email}' WHERE idUsuario={idUsuario};"
         
         cursor = connection.cursor()
-        cursor.execute(mySql_insert_query)
+        cursor.execute(updateDato)
         connection.commit()
         print(cursor.rowcount, "email actualizado") 
     except mysql.connector.Error as error:
@@ -27,15 +51,16 @@ def cambioDeEmail(email,idUsuario):
 #cambio de contraseña
 def cambioDeContraseña(password,idUsuario):
     try:
-        connection = mysql.connector.connect(host='localhost',
-        database='retrueque.sql',
-        user='admin',
-        password='admin')
+        connection = mysql.connector.connect(
+            host='localhost',
+            database='retrueque',
+            user='root',
+            password='')
 
-        mySql_insert_query = f" UPDATE password SET password ='{password}' WHERE idUsuario={idUsuario};"
+        updateDato = f" UPDATE email SET password ='{password}' WHERE idUsuario={idUsuario};"
         
         cursor = connection.cursor()
-        cursor.execute(mySql_insert_query)
+        cursor.execute(updateDato)
         connection.commit()
         print(cursor.rowcount, "contraseña actualizado") 
     except mysql.connector.Error as error:

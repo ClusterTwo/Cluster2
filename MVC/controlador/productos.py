e0d9069a552e0bb57c8c66c3f9b18b9b107cfc75
PABLO MATIAS JOSE MONTOYA <106557347+MontoyaPablo@users.noreply.github.com>
2022-11-12 10:53:43 -0300

    productos

--- MVC/controlador/productos.py
+++ MVC/controlador/productos.py
@@ -1,7 +1,30 @@
+from sys import path
+path.append('C:\\Users\\pablo\\AppData\\Local\\Programs\\Python\\Python310\\Lib\\site-packages')
+import mysql.connector 
 
 #instanciar un nuevo producto
+def cargaDeProducto(
+    descripcion,
+    categoria,
+    interesDeIntercambio
+):
+    connection=mysql.connector.connect(
+    host='localhost',
+    database='retrueque',
+    user='root',
+    password='')
+
+    cursor = connection.cursor()
+    insertDato = """INSERT INTO productos ( descripcion, categoria, interesDeIntercambio) 
+                                VALUES ( %s, %s, %s, %s, %s, %s) """
+    record = (  descripcion, categoria, interesDeIntercambio)
+    cursor.execute(insertDato, record)
+    connection.commit()
+    cursor.close()
+    connection.close()
 
 #eliminar posteo
+
 
 #concretar truque
 
